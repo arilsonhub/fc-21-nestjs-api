3d344e15f3ab79b23fd255c889c17bb16fd33aa3
Arilson Rosa <arilson.senac.ads@gmail.com>
2025-02-23 23:38:31 -0300

    Etapa 5 - Integração com Kafka

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,16 +4,18 @@ import { OrdersController } from './orders.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './entities/order.entity';
 import { OrdersGateway } from './orders.gateway';
+import { OrderConsumer } from './orders.consumer';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-    {
-      name: Order.name,
-      schema: OrderSchema
-    }])
+      {
+        name: Order.name,
+        schema: OrderSchema,
+      },
+    ]),
   ],
-  controllers: [OrdersController],
+  controllers: [OrdersController, OrderConsumer],
   providers: [OrdersService, OrdersGateway],
 })
 export class OrdersModule {}
